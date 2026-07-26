c968d3a5ebf454919a016c95d24892b6adab433c
vijayvar440 <vijayvar123456@gmail.com>
2026-07-26 23:18:48 +0530

    setup socst oi

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,11 +1,32 @@
 require("dotenv").config();
 
+const http = require("http");
+const { Server } = require("socket.io");
+
 const app = require("./src/app");
 const conectDB = require("./src/db/db");
 
 conectDB();
 
-app.listen(3000, ()=>{
-    console.log(" Server is runnig port 3000 ✅ ")
+const server = http.createServer(app);
 
-})
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"]
+    }
+});
+
+io.on("connection", (socket) => {
+
+    console.log("🟢 User Connected:", socket.id);
+
+    socket.on("disconnect", () => {
+        console.log("🔴 User Disconnected:", socket.id);
+    });
+
+});
+
+server.listen(3000, () => {
+    console.log("🚀 Server is running on port 3000");
+});
