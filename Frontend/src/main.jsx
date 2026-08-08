acc2da46d62fb8e26b86549b6b24e6b1c44bf031
vijayvar440 <vijayvar123456@gmail.com>
2026-08-08 18:13:03 +0530

    update

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./App.jsx";
 import { ThemeProvider } from "./ThemeContext";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <BrowserRouter>
