279086f866b7cc297d1f7b3df26af9bdc7b18e99
vijayvar440 <vijayvar123456@gmail.com>
2026-08-14 23:40:05 +0530

    update filr css

--- Frontend/src/pages/Post/CreatePost.jsx
+++ Frontend/src/pages/Post/CreatePost.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import "./CreatPost.css";
 
 function CreatePost() {
     const navigate = useNavigate();
