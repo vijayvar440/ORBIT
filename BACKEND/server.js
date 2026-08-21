1f763748ac37127279b6bb8cd9ca5aa1b33a799e
vijayvar440 <vijayvar123456@gmail.com>
2026-08-21 08:02:46 +0530

    Prepare backend for deployment

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -172,11 +172,8 @@ socket.on("message_seen", ({ messageId, senderId }) => {
 
 });
 
+const PORT = process.env.PORT || 3000;
 
-server.listen(3000, () => {
-
-    console.log(
-        "🚀 Server is running on port 3000"
-    );
-
+server.listen(PORT, "0.0.0.0", () => {
+    console.log(`🚀 Server is running on port ${PORT}`);
 });
