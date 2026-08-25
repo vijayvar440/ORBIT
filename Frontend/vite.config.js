dc34d4c8b867ea76886a369700e454feaa46195e
vijayvar440 <vijayvar123456@gmail.com>
2026-08-25 07:42:48 +0530

    Fix PWA icon paths

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -22,12 +22,12 @@ export default defineConfig({
 
                 icons: [
                     {
-                        src: "/icon/icon-192.png",
+                        src: "/icone/icon-192.png",
                         sizes: "192x192",
                         type: "image/png"
                     },
                     {
-                        src: "/icon/icon-512.png",
+                        src: "/icone/icon-512.png",
                         sizes: "512x512",
                         type: "image/png"
                     }
